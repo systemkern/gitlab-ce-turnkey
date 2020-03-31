35bb5e6bf5a653525c977ccdedf26ed689a876b0
himanchali <himamahi09@gmail.com>
2020-03-31 10:42:58 +0000

    Add database setup during startup

---
+++ wrapper_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -m
+
+# Start the first script that comes with gitlab image in background, as it never ends, it waits for all child process sigterm,
+# We can not run our script after checking it's exit status.
+
+/assets/wrapper &
+
+
+# Start the second script now
+/db_user_creation.sh
+
+#Bring the first process to foreground
+fg %1
